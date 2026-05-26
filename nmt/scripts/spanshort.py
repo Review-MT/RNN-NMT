0845700a07f0bd6e0aa1d89003a3c3ebf31fabdc
Review-MT <divya_1921cs10@gmail.com>
2026-05-26 10:47:35 +0530

    Remove utility scripts from remote tracking

--- nmt/scripts/spanshort.py
+++
@@ -1,9 +0,0 @@
-python -c "
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id='l3cube-pune/hindi-bert-v2',
-    local_dir='models/hindi-bert-v2',
-    local_dir_use_symlinks=False
-)
-"
